4e0750c8f663d98eaa132d9ae8f915c1d89192f6
kenneth <kenneth04@gmail.com>
2026-03-02 21:40:31 +0200

    prototype index.html complete

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,12 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def index():
     return render_template("index.html")
+
+@main.route("/signIn", methods=["GET", "POST"])
+def signin():
+    return render_template("signIn.html")
+
+
+@main.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
