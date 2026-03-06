cf18b6f4f3086d824a4fee88b1cae4b4a18b83dd
kenneth <kenneth04@gmail.com>
2026-03-06 19:17:24 +0200

    ...

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, redirect
+import time
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def index():
-    return render_template("chatScreen.html")
+    return render_template("index.html")
 
 @main.route("/signIn", methods=["GET", "POST"])
 def signin():
@@ -14,3 +15,7 @@ def signin():
 @main.route("/register", methods=["GET", "POST"])
 def register():
     return render_template("register.html")
+
+
+def chat():
+    return render_template("chatScreen.html")
