64f118a672b79a529bc8f34c0d7290f5bbacf286
kenneth <kenneth04@gmail.com>
2026-03-24 13:43:34 +0200

    removed Zone.Identifier files

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
-import eventlet
-eventlet.monkey_patch()
-
 from app import create_app, socketio
 
 app = create_app()
 
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+if __name__=="__main__":
+    socketio.run(app)
